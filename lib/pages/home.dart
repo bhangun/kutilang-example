fb8459f9f25eed57b3339a8d6dc84de0e11d435f
bhangun <bhangunh@gmail.com>
2021-06-07 12:57:14 +0700

    add connection

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
+import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kutilang_example/bloc/auth_bloc/auth.dart';
 import 'package:kutilang_example/bloc/auth_bloc/auth_bloc.dart';
@@ -17,9 +21,68 @@ class HomeScreen extends StatefulWidget {
 class _Homepagestate extends State<HomeScreen> {
   final _homeKey = GlobalKey<ScaffoldState>();
 
+    ConnectivityResult _connectionStatus = ConnectivityResult.none;
+  final Connectivity _connectivity = Connectivity();
+  late StreamSubscription<ConnectivityResult> _connectivitySubscription;
+  bool _isConnected = false;
+  String _connectionInfo = 'Disconnected!';
+
+  var subscription;
+
   @override
   void initState() {
     super.initState();
+  initConnectivity();
+
+    _connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
+  }
+
+  @override
+  void dispose() {
+    _connectivitySubscription.cancel();
+    super.dispose();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initConnectivity() async {
+    late ConnectivityResult result;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      result = await _connectivity.checkConnectivity();
+    } on PlatformException catch (e) {
+      print(e.toString());
+      return;
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) {
+      return Future.value(null);
+    }
+
+    return _updateConnectionStatus(result);
+  }
+
+  Future<void> _updateConnectionStatus(ConnectivityResult result) async {
+     if (result == ConnectivityResult.mobile) {
+      setState(() {
+        _connectionInfo = 'You are connected to mobile! ';
+        _isConnected = true;
+      });
+      
+    } else if (result == ConnectivityResult.wifi) {
+      setState(() {
+        _connectionInfo = 'You are connected to wifi! ';
+        _isConnected = true;
+      });
+    }else {
+    setState(() {
+       _connectionInfo = 'Offline ';
+       _isConnected = false;
+    });
+    }
   }
 
   @override
@@ -29,7 +92,7 @@ class _Homepagestate extends State<HomeScreen> {
       return Scaffold(
         key: _homeKey,
         appBar: KutAppBar(
-          title: 'Home',
+          title: 'Home '+ _isConnected.toString() + _connectionInfo,
           onLogout: ()=>test(),
         ),
         body: _buildBody(),
@@ -40,7 +103,6 @@ class _Homepagestate extends State<HomeScreen> {
   }
 
   void test() {
-   // print('-----logout-----');
     context.read<AuthBloc>().add(LoggedOut());
     //NavigationServices.navigateTo(AppsRoutes.login);
   }
